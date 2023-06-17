bc7d2cd0f95f494150c9011f69818b188040b18e
Moises-Drum <moises.drumand@gmail.com>
2023-06-17 15:19:23 -0400

    Adicionando primeira versão do projeto.

---
+++ Chamado/Servidor/servidor.py
@@ -0,0 +1,44 @@
+import threading
+import socket
+
+host = socket.gethostbyname(socket.gethostname())
+port = 55555
+
+clients = []                                                    #Esse é o servidor :3
+
+def main():
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+
+    try:
+        server.bind ((host , port))
+        server.listen(10)
+        print('\nServidor {} iniciado. Aguardando conexões...'.format(host))
+    except:
+        return print('\nNão foi possível iniciar o servidor!\n')
+
+    #loop esperando conexões
+    while True:
+        clientV, addr = server.accept()
+        clients.append(clientV)
+
+        thread = threading.Thread(target=messagesTreatment, args=[clientV])
+        thread.start()
+        print(f"[conexoes ativas] {threading.active_count() - 1}")
+
+#função que recebe as mensagens
+def messagesTreatment(clientV):
+    while True:
+        msg = clientV.recv(2048)
+        broadcast(msg, clientV)
+
+
+def broadcast(msg, clientV):
+    for clientItem in clients:
+        if clientItem != clientV:
+            clientItem.send(msg)
+                
+main()
+
---
+++ Chamado/Tecnico/tecnico.py
@@ -0,0 +1,48 @@
+import socket, json
+
+host = "127.0.1.1" # Deve ter o mesmo IP do servidor.
+port = 55555
+
+
+def main():
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client.connect((host, port))
+    except:                                                        #Partes da comunicação entre o servidor
+        return print('\nNão foi possívvel se conectar ao servidor!\n')
+
+    print('Bem vindo ao projeto ...')
+    print('Este é um ambiente para técnicos.\n')
+
+    print('Chamados abertos: \n')
+
+    while True:
+        msg = client.recv(2048).decode()        #Partes da comunicação entre o servidor
+        msg = eval(msg)
+
+        with open("bancodados.json", "w") as dAtua:
+            dAtua.write(json.dumps(msg, indent=2))
+                                                    #Partes da comunicação entre o arquivo json e o código
+        with open("bancodados.json", "r") as dAtua2:
+            msg2 = json.load(dAtua2)
+
+        for i in msg2:
+                nome =  msg2["Nome"]
+                cpf = msg2["CPF"]
+                cidade = msg2["Cidade"]
+                telefone = msg2["Telefone"]
+                setor = msg2["Setor"]
+                email = msg2["Email"]
+                situacao = msg2["Situacao"]
+        
+        print('Nome do solicitante: {}\n'.format(nome) + 
+                'CPF: {}\n'.format(cpf) +
+                'Cidade: {}\n'.format(cidade) +
+                'Telefone: {}\n'.format(telefone) +
+                'Setor: {}\n'.format(setor) + 
+                'E-mail: {}\n'.format(email) +
+                'Descrição do chamado: {}\n'.format(situacao))
+
+main()
